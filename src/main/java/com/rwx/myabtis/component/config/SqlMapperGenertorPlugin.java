f5791b774a8532fbe3950fa1fc2086b7b6d5d6a2
HelloJavaWorld123 <1806146700@qq.com>
2018-10-19 14:18:41 +0800

    修改部分代码

---
+++ src/main/java/com/rwx/myabtis/component/config/SqlMapperGenertorPlugin.java
@@ -0,0 +1,38 @@
+package com.rwx.myabtis.component.config;
+
+import org.mybatis.generator.api.GeneratedXmlFile;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.xml.Attribute;
+import org.mybatis.generator.api.dom.xml.Element;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+/**
+ * Author：RXK
+ * Date:2018/10/19 13:07
+ * Version: V1.0.0
+ * Des:
+ **/
+public class SqlMapperGenertorPlugin extends PluginAdapter {
+	@Override
+	public boolean validate(List<String> warnings) {
+		return true;
+	}
+
+
+	@Override
+	public boolean sqlMapGenerated(GeneratedXmlFile sqlMap, IntrospectedTable introspectedTable) {
+
+		try {
+			Field field = sqlMap.getClass().getDeclaredField("isMergeable");
+			field.setAccessible(true);
+			field.setBoolean(sqlMap, false);
+		} catch (NoSuchFieldException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+}
